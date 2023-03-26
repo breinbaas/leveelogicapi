dda7248410a7818b5080426964790051a205ec28
Rob van Putten <breinbaasnl@gmail.com>
2023-03-26 11:04:58 +0000

    added borehole api for pecto

--- server/routes/borehole.py
+++ server/routes/borehole.py
@@ -3,9 +3,11 @@ from fastapi.encoders import jsonable_encoder
 from pathlib import Path
 from io import BytesIO
 from starlette.responses import StreamingResponse
+import requests
 
 from leveelogic.objects.borehole import Borehole
 from leveelogic.objects.soillayer import SoilLayer
+from leveelogic.helpers import soilcode_to_parameters
 
 from ..const import ALLOWED_LOCATIONS
 
@@ -61,120 +63,121 @@ async def borehole_schema_to_borehole(borehole_schema: BoreholeSchema) -> Boreho
     return borehole
 
 
-# CREATE
-@router.post("/", response_description="Borehole data added into the database")
-async def create_borehole_data(borehole: BoreholeSchema = Body(...)):
-    borehole = jsonable_encoder(borehole)
+# # CREATE
+# @router.post("/", response_description="Borehole data added into the database")
+# async def create_borehole_data(borehole: BoreholeSchema = Body(...)):
+#     borehole = jsonable_encoder(borehole)
 
-    if borehole["location"] not in ALLOWED_LOCATIONS:
-        return ErrorResponseModel(
-            "An error occurred.",
-            404,
-            f"Unsupported location '{borehole['location']}' found (options are 'crest', 'polder', 'both')",
-        )
+#     if borehole["location"] not in ALLOWED_LOCATIONS:
+#         return ErrorResponseModel(
+#             "An error occurred.",
+#             404,
+#             f"Unsupported location '{borehole['location']}' found (options are 'crest', 'polder', 'both')",
+#         )
 
-    new_borehole = await create_borehole(borehole)
-    return ResponseModel(new_borehole, "Borehole added successfully.")
-
-
-@router.post(
-    "/upload/", response_description="Borehole data uploaded into the database"
-)
-async def create_upload_file(file: UploadFile, location: str = "both"):
-    if location not in ALLOWED_LOCATIONS:
-        return ErrorResponseModel(
-            "An error occurred.",
-            404,
-            f"Unsupported location '{location}' found (options are 'crest', 'polder', 'both')",
-        )
-
-    try:
-        borehole = await upload_file_to_borehole(file)
-    except Exception as e:
-        return ErrorResponseModel("An error occurred.", 404, str(e))
-
-    # done, create a borehole schema to add to the database
-    borehole_schema = BoreholeSchema(
-        name=borehole.name,
-        date=borehole.date,
-        location=location,
-        lat=borehole.lat,
-        lon=borehole.lon,
-        top=borehole.top,
-        bottom=borehole.bottom,
-        soillayers=[sl.dict() for sl in borehole.soillayers],
-    )
-    borehole_schema = jsonable_encoder(borehole_schema)
-    new_borehole = await create_borehole(borehole_schema)
-    return ResponseModel(new_borehole, "Borehole added successfully.")
+#     new_borehole = await create_borehole(borehole)
+#     return ResponseModel(new_borehole, "Borehole added successfully.")
 
 
-# RETRIEVE
-@router.get("/", response_description="Boreholes retrieved")
-async def get_boreholes():
-    boreholes = await retrieve_boreholes()
-    if boreholes:
-        return ResponseModel(boreholes, "Boreholes data retrieved successfully")
-    return ResponseModel(boreholes, "Empty list returned")
+# @router.post(
+#     "/upload/", response_description="Borehole data uploaded into the database"
+# )
+# async def create_upload_file(file: UploadFile, location: str = "both"):
+#     if location not in ALLOWED_LOCATIONS:
+#         return ErrorResponseModel(
+#             "An error occurred.",
+#             404,
+#             f"Unsupported location '{location}' found (options are 'crest', 'polder', 'both')",
+#         )
+
+#     try:
+#         borehole = await upload_file_to_borehole(file)
+#     except Exception as e:
+#         return ErrorResponseModel("An error occurred.", 404, str(e))
+
+#     # done, create a borehole schema to add to the database
+#     borehole_schema = BoreholeSchema(
+#         name=borehole.name,
+#         date=borehole.date,
+#         location=location,
+#         lat=borehole.lat,
+#         lon=borehole.lon,
+#         top=borehole.top,
+#         bottom=borehole.bottom,
+#         soillayers=[sl.dict() for sl in borehole.soillayers],
+#     )
+#     borehole_schema = jsonable_encoder(borehole_schema)
+#     new_borehole = await create_borehole(borehole_schema)
+#     return ResponseModel(new_borehole, "Borehole added successfully.")
 
 
-@router.get("/{id}", response_description="Borehole data retrieved")
-async def get_borehole_data(id):
-    try:
-        borehole = await retrieve_borehole(id)
-    except Exception as e:
-        return ErrorResponseModel("An error occurred.", 404, str(e))
-    if borehole:
-        return ResponseModel(borehole, "Borehole data retrieved successfully")
-    return ErrorResponseModel("An error occurred.", 404, "Borehole doesn't exist.")
+# # RETRIEVE
+# @router.get("/", response_description="Boreholes retrieved")
+# async def get_boreholes():
+#     boreholes = await retrieve_boreholes()
+#     if boreholes:
+#         return ResponseModel(boreholes, "Boreholes data retrieved successfully")
+#     return ResponseModel(boreholes, "Empty list returned")
 
 
-# UPDATE
-@router.put("/{id}")
-async def update_borehole_data(id: str, req: UpdateBoreholeModel = Body(...)):
-    req = {k: v for k, v in req.dict().items() if v is not None}
-
-    if "location" in req.keys():
-        if not req["location"] in ALLOWED_LOCATIONS:
-            return ErrorResponseModel(
-                "An error occurred.",
-                404,
-                f"Unsupported location '{req['location']}' found (options are 'crest', 'polder', 'both')",
-            )
-
-    try:
-        updated_borehole = await update_borehole(id, req)
-    except Exception as e:
-        return ErrorResponseModel("An error occurred.", 404, str(e))
-    if updated_borehole:
-        return ResponseModel(
-            f"Borehole with ID: {id} name update is successful",
-            "Borehole updated successfully",
-        )
-    return ErrorResponseModel("An error occurred.", 404, "Borehole doesn't exist.")
+# @router.get("/{id}", response_description="Borehole data retrieved")
+# async def get_borehole_data(id):
+#     try:
+#         borehole = await retrieve_borehole(id)
+#     except Exception as e:
+#         return ErrorResponseModel("An error occurred.", 404, str(e))
+#     if borehole:
+#         return ResponseModel(borehole, "Borehole data retrieved successfully")
+#     return ErrorResponseModel("An error occurred.", 404, "Borehole doesn't exist.")
 
 
-# DELETE
-@router.delete("/{id}", response_description="Borehole data deleted from the database")
-async def delete_borehole_data(id: str):
-    try:
-        deleted_borehole = await delete_borehole(id)
-    except Exception as e:
-        return ErrorResponseModel("An error occurred.", 404, str(e))
-    if deleted_borehole:
-        return ResponseModel(
-            f"Borehole with ID: {id} removed", "Borehole deleted successfully"
-        )
-    return ErrorResponseModel(
-        "An error occurred", 404, f"Borehole with id {id} doesn't exist"
-    )
+# # UPDATE
+# @router.put("/{id}")
+# async def update_borehole_data(id: str, req: UpdateBoreholeModel = Body(...)):
+#     req = {k: v for k, v in req.dict().items() if v is not None}
+
+#     if "location" in req.keys():
+#         if not req["location"] in ALLOWED_LOCATIONS:
+#             return ErrorResponseModel(
+#                 "An error occurred.",
+#                 404,
+#                 f"Unsupported location '{req['location']}' found (options are 'crest', 'polder', 'both')",
+#             )
+
+#     try:
+#         updated_borehole = await update_borehole(id, req)
+#     except Exception as e:
+#         return ErrorResponseModel("An error occurred.", 404, str(e))
+#     if updated_borehole:
+#         return ResponseModel(
+#             f"Borehole with ID: {id} name update is successful",
+#             "Borehole updated successfully",
+#         )
+#     return ErrorResponseModel("An error occurred.", 404, "Borehole doesn't exist.")
+
+
+# # DELETE
+# @router.delete("/{id}", response_description="Borehole data deleted from the database")
+# async def delete_borehole_data(id: str):
+#     try:
+#         deleted_borehole = await delete_borehole(id)
+#     except Exception as e:
+#         return ErrorResponseModel("An error occurred.", 404, str(e))
+#     if deleted_borehole:
+#         return ResponseModel(
+#             f"Borehole with ID: {id} removed", "Borehole deleted successfully"
+#         )
+#     return ErrorResponseModel(
+#         "An error occurred", 404, f"Borehole with id {id} doesn't exist"
+#     )
 
 # PLOT FROM UPLOAD
 @router.post(
-    "/plot/", response_description="Plot of the uploaded Borehole data with the Robertson correlation"
+    "/plot/",
+    response_description="Plot of the uploaded Borehole data with the Robertson correlation",
 )
 async def plot_from_upload(
-    file: UploadFile,    
+    file: UploadFile,
 ):
     try:
         borehole = await upload_file_to_borehole(file)
@@ -186,3 +189,37 @@ async def plot_from_upload(
         return ErrorResponseModel("An error occurred.", 404, str(e))
 
     return StreamingResponse(buf, media_type="image/png")
+
+
+#########
+# PECTO #
+#########
+
+# CLASSIFY FROM URL
+# TEST URL: https://app.pecto.nl/app/static/wsrl/safe/go_safe/totaal_gef/VY095.+004_HB_BIT.gef
+@router.post("/classify_from_url/", response_description="Borehole data as soillayers")
+async def classify_from_url(url: str, include_color: bool = True):
+    try:
+        response = requests.get(url)
+        data = response.text
+        borehole = Borehole.from_string(data, ".gef")
+
+        # compress the layers so layers with the same name are put into one layer
+        borehole.compress()
+
+        # use soilcode_to_parameters from helpers
+        layer_response = [layer.dict() for layer in borehole.soillayers]
+
+        if include_color:
+            for layer_dict in layer_response:
+                layer_dict["color"] = soilcode_to_parameters(layer_dict["soilcode"])[
+                    "color"
+                ]
+
+    except Exception as e:
+        return ErrorResponseModel("An error occurred.", 404, str(e))
+
+    return ResponseModel(
+        layer_response,
+        "Borehole soillayers",
+    )
--- server/routes/cpt.py
+++ server/routes/cpt.py
@@ -64,114 +64,114 @@ async def cpt_schema_to_cpt(cpt_schema: CptSchema) -> Cpt:
     return cpt
 
 
-# CREATE
-@router.post("/", response_description="Cpt data added into the database")
-async def create_cpt_data(cpt: CptSchema = Body(...)):
-    cpt = jsonable_encoder(cpt)
+# # CREATE
+# @router.post("/", response_description="Cpt data added into the database")
+# async def create_cpt_data(cpt: CptSchema = Body(...)):
+#     cpt = jsonable_encoder(cpt)
 
-    if cpt["location"] not in ALLOWED_LOCATIONS:
-        return ErrorResponseModel(
-            "An error occurred.",
-            404,
-            f"Unsupported location '{cpt['location']}' found (options are 'crest', 'polder', 'both')",
-        )
+#     if cpt["location"] not in ALLOWED_LOCATIONS:
+#         return ErrorResponseModel(
+#             "An error occurred.",
+#             404,
+#             f"Unsupported location '{cpt['location']}' found (options are 'crest', 'polder', 'both')",
+#         )
 
-    new_cpt = await create_cpt(cpt)
-    return ResponseModel(new_cpt, "Cpt added successfully.")
-
-
-@router.post("/upload/", response_description="Cpt data uploaded into the database")
-async def create_upload_file(file: UploadFile, location: str = "both"):
-    if location not in ALLOWED_LOCATIONS:
-        return ErrorResponseModel(
-            "An error occurred.",
-            404,
-            f"Unsupported location '{location}' found (options are 'crest', 'polder', 'both')",
-        )
-
-    try:
-        cpt = await upload_file_to_cpt(file)
-    except Exception as e:
-        return ErrorResponseModel("An error occurred.", 404, str(e))
-
-    # done, create a cpt schema to add to the database
-    cpt_schema = CptSchema(
-        name=cpt.name,
-        date=cpt.date,
-        location=location,
-        lat=cpt.lat,  # cpt.lat
-        lon=cpt.lon,  # cpt.lon
-        top=cpt.top,
-        bottom=cpt.bottom,
-        pre_excavated_depth=cpt.pre_excavated_depth,
-        zs=[round(z, 3) for z in cpt.z],
-        qc=[round(z, 3) for z in cpt.qc],
-        fs=[round(z, 5) for z in cpt.fs],
-        fr=[round(z, 3) for z in cpt.fr],
-        u2=[round(z, 5) for z in cpt.u],
-    )
-    cpt_schema = jsonable_encoder(cpt_schema)
-    new_cpt = await create_cpt(cpt_schema)
-    return ResponseModel(new_cpt, "Cpt added successfully.")
+#     new_cpt = await create_cpt(cpt)
+#     return ResponseModel(new_cpt, "Cpt added successfully.")
 
 
-# RETRIEVE
-@router.get("/", response_description="Cpts retrieved")
-async def get_cpts():
-    cpts = await retrieve_cpts()
-    if cpts:
-        return ResponseModel(cpts, "Cpts data retrieved successfully")
-    return ResponseModel(cpts, "Empty list returned")
+# @router.post("/upload/", response_description="Cpt data uploaded into the database")
+# async def create_upload_file(file: UploadFile, location: str = "both"):
+#     if location not in ALLOWED_LOCATIONS:
+#         return ErrorResponseModel(
+#             "An error occurred.",
+#             404,
+#             f"Unsupported location '{location}' found (options are 'crest', 'polder', 'both')",
+#         )
+
+#     try:
+#         cpt = await upload_file_to_cpt(file)
+#     except Exception as e:
+#         return ErrorResponseModel("An error occurred.", 404, str(e))
+
+#     # done, create a cpt schema to add to the database
+#     cpt_schema = CptSchema(
+#         name=cpt.name,
+#         date=cpt.date,
+#         location=location,
+#         lat=cpt.lat,  # cpt.lat
+#         lon=cpt.lon,  # cpt.lon
+#         top=cpt.top,
+#         bottom=cpt.bottom,
+#         pre_excavated_depth=cpt.pre_excavated_depth,
+#         zs=[round(z, 3) for z in cpt.z],
+#         qc=[round(z, 3) for z in cpt.qc],
+#         fs=[round(z, 5) for z in cpt.fs],
+#         fr=[round(z, 3) for z in cpt.fr],
+#         u2=[round(z, 5) for z in cpt.u],
+#     )
+#     cpt_schema = jsonable_encoder(cpt_schema)
+#     new_cpt = await create_cpt(cpt_schema)
+#     return ResponseModel(new_cpt, "Cpt added successfully.")
 
 
-@router.get("/{id}", response_description="Cpt data retrieved")
-async def get_cpt_data(id):
-    try:
-        cpt = await retrieve_cpt(id)
-    except Exception as e:
-        return ErrorResponseModel("An error occurred.", 404, str(e))
-    if cpt:
-        return ResponseModel(cpt, "Cpt data retrieved successfully")
-    return ErrorResponseModel("An error occurred.", 404, "Cpt doesn't exist.")
+# # RETRIEVE
+# @router.get("/", response_description="Cpts retrieved")
+# async def get_cpts():
+#     cpts = await retrieve_cpts()
+#     if cpts:
+#         return ResponseModel(cpts, "Cpts data retrieved successfully")
+#     return ResponseModel(cpts, "Empty list returned")
 
 
-# UPDATE
-@router.put("/{id}")
-async def update_cpt_data(id: str, req: UpdateCptModel = Body(...)):
-    req = {k: v for k, v in req.dict().items() if v is not None}
-
-    if "location" in req.keys():
-        if not req["location"] in ALLOWED_LOCATIONS:
-            return ErrorResponseModel(
-                "An error occurred.",
-                404,
-                f"Unsupported location '{req['location']}' found (options are 'crest', 'polder', 'both')",
-            )
-
-    try:
-        updated_cpt = await update_cpt(id, req)
-    except Exception as e:
-        return ErrorResponseModel("An error occurred.", 404, str(e))
-    if updated_cpt:
-        return ResponseModel(
-            f"Cpt with ID: {id} name update is successful",
-            "Cpt updated successfully",
-        )
-    return ErrorResponseModel("An error occurred.", 404, "Cpt doesn't exist.")
+# @router.get("/{id}", response_description="Cpt data retrieved")
+# async def get_cpt_data(id):
+#     try:
+#         cpt = await retrieve_cpt(id)
+#     except Exception as e:
+#         return ErrorResponseModel("An error occurred.", 404, str(e))
+#     if cpt:
+#         return ResponseModel(cpt, "Cpt data retrieved successfully")
+#     return ErrorResponseModel("An error occurred.", 404, "Cpt doesn't exist.")
 
 
-# DELETE
-@router.delete("/{id}", response_description="Cpt data deleted from the database")
-async def delete_cpt_data(id: str):
-    try:
-        deleted_cpt = await delete_cpt(id)
-    except Exception as e:
-        return ErrorResponseModel("An error occurred.", 404, str(e))
-    if deleted_cpt:
-        return ResponseModel(f"Cpt with ID: {id} removed", "Cpt deleted successfully")
-    return ErrorResponseModel(
-        "An error occurred", 404, f"Cpt with id {id} doesn't exist"
-    )
+# # UPDATE
+# @router.put("/{id}")
+# async def update_cpt_data(id: str, req: UpdateCptModel = Body(...)):
+#     req = {k: v for k, v in req.dict().items() if v is not None}
+
+#     if "location" in req.keys():
+#         if not req["location"] in ALLOWED_LOCATIONS:
+#             return ErrorResponseModel(
+#                 "An error occurred.",
+#                 404,
+#                 f"Unsupported location '{req['location']}' found (options are 'crest', 'polder', 'both')",
+#             )
+
+#     try:
+#         updated_cpt = await update_cpt(id, req)
+#     except Exception as e:
+#         return ErrorResponseModel("An error occurred.", 404, str(e))
+#     if updated_cpt:
+#         return ResponseModel(
+#             f"Cpt with ID: {id} name update is successful",
+#             "Cpt updated successfully",
+#         )
+#     return ErrorResponseModel("An error occurred.", 404, "Cpt doesn't exist.")
+
+
+# # DELETE
+# @router.delete("/{id}", response_description="Cpt data deleted from the database")
+# async def delete_cpt_data(id: str):
+#     try:
+#         deleted_cpt = await delete_cpt(id)
+#     except Exception as e:
+#         return ErrorResponseModel("An error occurred.", 404, str(e))
+#     if deleted_cpt:
+#         return ResponseModel(f"Cpt with ID: {id} removed", "Cpt deleted successfully")
+#     return ErrorResponseModel(
+#         "An error occurred", 404, f"Cpt with id {id} doesn't exist"
+#     )
 
 
 # CLASSIFICATION FROM DATABASE
@@ -256,6 +256,7 @@ async def plot_from_upload(
 #########
 
 # PLOT FROM PECTO URL
+# TEST URL: https://app.pecto.nl/app/static/wsrl/safe/go_safe/totaal_gef/AW272.+100_DKMP2_BIT.gef
 @router.post(
     "/plot_from_url/",
     response_description="base64 string for the plot of the Cpt data with the Robertson correlation from a given URL",
