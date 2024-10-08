b96a6146a6607f36c7b0b00fc74dc45c5893ad49
Rob van Putten <breinbaasnl@gmail.com>
2024-10-08 12:56:38 +0200

    added cpt to json function

--- api/api_v1/endpoints/cpts.py
+++ api/api_v1/endpoints/cpts.py
@@ -1,10 +1,27 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, UploadFile, HTTPException
 from typing import Annotated
+from leveelogic.objects.cpt import Cpt
+from pathlib import Path
+
 from ...security import User, get_current_active_user
 
 router = APIRouter()
 
 
 @router.get("/ping")
-async def pong(current_user: Annotated[User, Depends(get_current_active_user)],):
+async def pong(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
     return {"ping": "pong from cpts!"}
+
+
+@router.post("/to_json")
+async def cpt_to_json(file: UploadFile):
+    contents = await file.read()
+    suffix = Path(file.filename).suffix.lower()
+    try:
+        s_utf8 = contents.decode("utf-8", errors="ignore")
+        cpt = Cpt.from_string(s_utf8, suffix=suffix)
+        return cpt
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Error reading file '{e}'")
