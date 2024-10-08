af8ab1253316a878efe178b6c5ff2706b3e34374
Rob van Putten <breinbaasnl@gmail.com>
2024-10-08 11:05:06 +0200

    adding security

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sqlite3 /workspace <<'END_SQL'
+sqlite3 /workspace/api.db <<'END_SQL'
 .timeout 2000
 CREATE TABLE if not exists Users(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, email TEXT NOT NULL, password TEXT NOT NULL, role CHAR(10) NOT NULL, disabled BOOL NOT NULL);
 END_SQL
