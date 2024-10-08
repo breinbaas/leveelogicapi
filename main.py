74448ca87450252285b1c18635132ac65cc75836
Rob van Putten <breinbaasnl@gmail.com>
2024-10-08 10:26:35 +0200

    gunicorn correction

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import uvicorn
+
+# from api.api_v1.api import router as api_v1_router
+
+
+app = FastAPI()
+
+# app.include_router(api_v1_router, prefix="/api/v1")
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+if __name__=="__main__":
+    uvicorn.run("main:app", port=8000, reload=True)
